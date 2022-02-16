c2c26adf87e1cb74e411860fdceb55c8ba64f75b
agholson <87455899+agholson@users.noreply.github.com>
2022-02-15 22:02:22 -0500

    Initial Commit

---
+++ DogsApp/DogsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsAppApp.swift
+//  DogsApp
+//
+//  Created by Leone on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct DogsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
